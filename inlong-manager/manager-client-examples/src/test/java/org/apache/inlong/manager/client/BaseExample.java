f546262091b1c586d5545178bca176b6c5bb6a63
woofyzhao <490467089@qq.com>
2022-06-13 11:59:21 +0800

    v

--- inlong-manager/manager-client-examples/src/test/java/org/apache/inlong/manager/client/BaseExample.java
+++ inlong-manager/manager-client-examples/src/test/java/org/apache/inlong/manager/client/BaseExample.java
@@ -50,9 +50,9 @@ public class BaseExample {
     // Inlong user && passwd
     private DefaultAuthentication inlongAuth = new DefaultAuthentication("admin", "inlong");
     // Inlong group ID
-    private String groupId = "g_0613_106";
+    private String groupId = "g_0613_107";
     // Inlong stream ID
-    private String streamId = "s_0613_106";
+    private String streamId = "s_0613_107";
     // Flink cluster url
     private String flinkUrl = "";
     // Pulsar cluster admin url
@@ -62,9 +62,9 @@ public class BaseExample {
     // Pulsar tenant
     private String tenant = "public";
     // Pulsar namespace
-    private String namespace = "g_0613_106";
+    private String namespace = "g_0613_107";
     // Pulsar topic
-    private String topic = "s_0613_106";
+    private String topic = "s_0613_107";
 
     /**
      * Create inlong group info
